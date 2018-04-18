f21eb98dd3a8ace5c49116b276c2681694fe2a00
Kevin <kevin_01fcc@hotmail.com>
2018-04-18 12:39:41 -0500

    Variables y json.

---
+++ JavaScript-2/Variables.js
@@ -0,0 +1,34 @@
+console.log('Hola mundo');
+var mutar;
+//Json
+var usuario = {
+    nombre: 'Kevin',
+    apellido: 'Carate',
+    cedula:'1234567890',
+    edad:'23',
+};
+
+console.log(usuario.nombre, usuario.apellido);
+console.log(usuario);
+
+/*
+var nombre = 'Kevin';
+mutar = nombre;
+var edad = 23;
+mutar = edad;
+var peso = 40.5;
+var casado = false;
+mutar = casado;
+var fechaNacimiento = new Date();
+var noExisto = null;//False
+var noEstoyDefinido = undefined;//False
+var num = 0; //False
+var num1 = 1; //True
+var num2 = -1; //True
+
+if (num1) {
+    console.log('verdadero')
+} else {
+    console.log('falso')
+}
+*/
